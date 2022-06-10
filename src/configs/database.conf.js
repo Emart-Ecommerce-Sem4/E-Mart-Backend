b18cecdc35da06f549fe9c07d50e72904ee28142
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-06-10 18:58:55 +0530

    Host database added

--- src/configs/database.conf.js
+++ src/configs/database.conf.js
@@ -1,11 +1,15 @@
 const { Pool } = require("pg");
+const fs = require("fs");
 
 const pool = new Pool({
-  user: "database_user",
-  database: "dbms",
-  password: "password@123",
-  port: 5432,
-  host: "localhost",
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_USER_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  host: process.env.DATABASE_HOST,
+  ssl: {
+    ca: fs.readFileSync(__dirname + "/ssl/certificate.crt.pem"),
+  },
 });
 
 module.exports = pool;
