f1377763908cebc70c0ff5a5b8de0c8b7628c189
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-06-24 10:54:41 +0530

    category deletion added

--- src/repositories/categoryRepository.js
+++ src/repositories/categoryRepository.js
@@ -3,6 +3,27 @@ const {
   InternalServerErrorException,
 } = require("../exceptions/InternalServerErrorException");
 
+async function getAllCategories() {
+  try {
+    const res = await pool.query("SELECT * FROM category");
+    return res;
+  } catch (error) {
+    throw new InternalServerErrorException();
+  }
+}
+
+async function getCategoryById(id) {
+  try {
+    const res = await pool.query(
+      "SELECT * from category WHERE category_id = $1",
+      [id]
+    );
+    return res;
+  } catch (error) {
+    throw new InternalServerErrorException();
+  }
+}
+
 async function getCategoryByName(name) {
   try {
     const res = await pool.query(
@@ -28,4 +49,23 @@ async function addCategory(values) {
   }
 }
 
-module.exports = { addCategory, getCategoryByName };
+async function deleteCategory(id) {
+  try {
+    const res = await pool.query(
+      "DELETE FROM category WHERE category_id = $1",
+      [id]
+    );
+
+    return res;
+  } catch (error) {
+    throw new InternalServerErrorException();
+  }
+}
+
+module.exports = {
+  addCategory,
+  getCategoryByName,
+  getCategoryById,
+  getAllCategories,
+  deleteCategory,
+};
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -7,6 +7,48 @@ const {
   InternalServerErrorException,
 } = require("../exceptions/InternalServerErrorException");
 
+async function getAllCategories() {
+  try {
+    const res = await categoryRepository.getAllCategories();
+    return generateOutput(200, "All categories succesfully received", res.rows);
+  } catch (error) {
+    if (error instanceof InternalServerErrorException) {
+      // Internal server error exception
+      return generateOutput(
+        500,
+        "Error in getting the categories",
+        error.message
+      );
+    }
+    return generateOutput(
+      400,
+      "Error in getting the categories",
+      "An error occured!"
+    );
+  }
+}
+
+async function getCategory(id) {
+  try {
+    const res = await categoryRepository.getCategoryById(id);
+    return generateOutput(200, "Category getting sucess", res.rows);
+  } catch (error) {
+    if (error instanceof InternalServerErrorException) {
+      // Internal server error exception
+      return generateOutput(
+        500,
+        "Error in getting the category",
+        error.message
+      );
+    }
+    return generateOutput(
+      400,
+      "Error in getting the category",
+      "An error occured!"
+    );
+  }
+}
+
 async function addCategory(data) {
   if (!data?.categoryName) {
     return generateOutput(400, "Validation Error", {
@@ -53,4 +95,25 @@ async function addCategory(data) {
   }
 }
 
-module.exports = { addCategory };
+async function deleteCategory(id) {
+  try {
+    const res = await categoryRepository.deleteCategory(id);
+    return generateOutput(200, "Category deleted succesfully!");
+  } catch (error) {
+    if (error instanceof InternalServerErrorException) {
+      // Internal server error exception
+      return generateOutput(
+        500,
+        "Error in deleting the category",
+        error.message
+      );
+    }
+    return generateOutput(
+      400,
+      "Error in deleting the category",
+      "An error occured!"
+    );
+  }
+}
+
+module.exports = { addCategory, getCategory, getAllCategories, deleteCategory };
