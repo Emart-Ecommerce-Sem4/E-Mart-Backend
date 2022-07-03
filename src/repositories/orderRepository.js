2bc8556c323a6b15c43c9ce7133c61ad9fce8d1b
SumeelaMadusankha <hasumeela@gmail.com>
2022-07-04 01:05:46 +0530

    add order edited

--- src/repositories/orderRepository.js
+++ src/repositories/orderRepository.js
@@ -4,7 +4,9 @@ const {
 } = require("../exceptions/InternalServerErrorException");
 
 async function addOrder(values) {
+ 
   try {
+     pool.query("BEGIN");
     const res = await pool.query(
       "INSERT INTO user_orders (order_id,user_id,order_date,item_count,order_status,comments,payment_method,total_price) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)",
       [
@@ -18,8 +20,23 @@ async function addOrder(values) {
         values.totalPrice,
       ]
     );
+
+    values.products.forEach(async element =>  {
+   
+      const res1 = await pool.query(
+        "INSERT INTO order_items (order_id,product_id,item_count,total_price) VALUES ($1,$2,$3,$4)",
+        [
+          values.id,
+          element.product_id,
+          element.item_count,
+          element.totalPrice
+        ]
+      );
+    });
+    await pool.query("COMMIT");
     return true;
   } catch (error) {
+    await pool.query("ROLLBACK");
     console.log(error);
     throw InternalServerErrorException();
   }
