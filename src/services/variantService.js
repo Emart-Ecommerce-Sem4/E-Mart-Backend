1517e675c7e67994917f8987afaa963340ce8b5d
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-07-06 14:41:01 +0530

    Additional oce removed

--- src/services/variantService.js
+++ src/services/variantService.js
@@ -66,27 +66,6 @@ async function addVariant(values) {
       "An error occured!"
     );
   }
-  const variant = {
-    id: uuid.v4(),
-    type: values.type,
-  };
-  try {
-    const res = await variantRepository.addVariant(variant);
-    return generateOutput(201, "Variant added succesfully!", {
-      variant,
-    });
-  } catch (error) {
-    if (error instanceof InternalServerErrorException) {
-      // Internal server error exception
-
-      return generateOutput(500, "Error in adding the variant", error.message);
-    }
-    return generateOutput(
-      400,
-      "Error in adding the variant",
-      "An error occured!"
-    );
-  }
 }
 
 async function updateVariant(values) {
